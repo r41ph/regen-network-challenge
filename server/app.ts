38c540b6c6837aca7199da87a3993051264fdd51
blushi <marie.gauthier63@gmail.com>
2024-02-07 10:08:44 +0100

    chore: add cors

--- server/app.ts
+++ server/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
-
+import cors from 'cors';
 import projects from './routes/projects';
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.use('/projects', projects);
 app.listen(port);
